42438e46edad4671b0564fcfb09de2071b070ee6
Isaac Johnston <isaac@superstruct.nz>
2025-10-14 21:43:53 +1300

    fix: substitute TypeScript template variables with actual library names

--- bench/glib.bench.ts
+++ bench/glib.bench.ts
@@ -1,12 +1,12 @@
 /**
- * ${LIB_TITLE} WASM Benchmarks
+ * Glib WASM Benchmarks
  */
 
-import ${LIB_TITLE}WASM from "../src/lib/index.ts"
+import GlibWASM from "../src/lib/index.ts"
 
-Deno.bench("${LIB_NAME} initialization", {
+Deno.bench("glib initialization", {
   baseline: true
 }, async () => {
-  const lib = new ${LIB_TITLE}WASM()
+  const lib = new GlibWASM()
   await lib.initialize()
 })
